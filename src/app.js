aa6afcc99bd24629504ac05768d5768688c77b94
raihaneong <raihneong@gmail.com>
2022-06-17 19:50:50 +0700

    naming the Appjs className

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header'
 
 export default function App() {
     return(
-        <div>
+        <div className="App">
             < Header />
         </div>
     )
