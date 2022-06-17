55f789d3b99f1cb91d091ba6dd461c8d7c002af4
raihaneong <raihneong@gmail.com>
2022-06-17 19:41:41 +0700

    change the appjs text to component Header

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import Header from './components/Header'
 
 export default function App() {
     return(
-        <h1>
-            test
-        </h1>
+        <div>
+            < Header />
+        </div>
     )
 }
