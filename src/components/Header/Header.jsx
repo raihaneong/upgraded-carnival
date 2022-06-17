491e5c03f1678c0a3f0375c3b92319556950f91c
raihaneong <raihneong@gmail.com>
2022-06-17 19:49:08 +0700

    wrapping Header content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,10 @@ import './Header.scss';
 
 export default function Header() {
     return(
-        <h1>
-            test
-        </h1>
+        <div>
+            <h1>
+                test
+            </h1>
+        </div>
     )
 }
